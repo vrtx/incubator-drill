65bf605b284dea02034db37f8c0613c79cc4f966
Ben Becker <benjamin.becker@gmail.com>
2013-10-14 14:19:08 -0700

    cleanup.  tests pass with MergingRecordBatch stub.

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/mergereceiver/TestMergingReceiver.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/mergereceiver/TestMergingReceiver.java
@@ -1,4 +1,3 @@
-package org.apache.drill.exec.physical.impl.mergereceiver;
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,6 +15,8 @@ package org.apache.drill.exec.physical.impl.mergereceiver;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.drill.exec.physical.impl.mergereceiver;
 
 import java.util.List;
 
